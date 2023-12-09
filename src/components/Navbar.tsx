f0a2a7e2d663fcd04673ae4a11caa33b35aa0ac0
groh7 <pawel.pogroszewski1@wp.pl>
2023-12-09 21:37:37 +0100

    commit style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,15 +11,15 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center p-4">
       <div className="flex items-center">
-        <div className="w-80">
-          <Link href="/">Logo</Link>
+        <div className="w-80 ">
+          <Link className="text-white" href="/">SKLEP</Link>
         </div>
       </div>
       <div className="flex items-center space-x-4">
         <Link href="/cart" className="relative">
           <div>
-          <span className="absolute top-0 right-0 bg-red-500 text-white rounded-full w-4 h-4 flex items-center justify-center">(
-            <i><FiShoppingCart size={24} /></i>
+          <span className="cart text-right text-white flex items-center justify-center">
+            (<i><FiShoppingCart size={24} /></i>
             {cart.length})
           </span>
           </div>
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRecoilState } from 'recoil';
-import { cartState } from "../atoms/cartState";
+import { cartState } from '../atoms/cartState';
 
 type ProductPropsType = {
   product: {
@@ -22,28 +22,27 @@ function Product({ product }: ProductPropsType) {
     if (productIndex === -1) {
       setCart([...cart, product]);
     } else {
-      setCart(prevCart => prevCart.map(item =>
-        item.id === product.id ? { ...item, quantity: (item.quantity || 1) + 1 } : item
-      ));
+      setCart(prevCart =>
+        prevCart.map(item =>
+          item.id === product.id ? { ...item, quantity: (item.quantity || 1) + 1 } : item
+        )
+      );
     }
-
-
   };
 
   return (
-      <div className="flex flex-col border p-4 mb-4">
-        <h2 className="text-xl font-bold mb-2">{product.name}</h2>
-        <p className="text-gray-600 mb-4">{product.description}</p>
+    <div className="w-1/4 p-4">
+      <div className="product flex flex-col border p-4 mb-4">
+        <h2 className="text-xl font-bold mb-2 text-center">{product.name}</h2>
+        <p className="text-gray-600 mb-4 text-center">{product.description}</p>
         <button
           onClick={addItemsToCart}
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none"
-        >
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none">
           Add to Cart
         </button>
-        <hr className="my-4 border-t" />
       </div>
-    );
-  };
-
+    </div>
+  );
+}
 
 export default Product;
