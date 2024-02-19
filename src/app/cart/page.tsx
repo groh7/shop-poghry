f7ad041f04a240420064e7c4a28e189afe39dd2e
Grzegorz Sierocki <grzessierocki@gmail.com>
2024-02-19 22:13:03 +0100

    handle delete in local storage

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { useShopContext } from "@/providers/ShopContext";
 import { ProductType } from "@/models/product";
+import { useShopContext } from "@/providers/ShopContext";
+import React, { useEffect, useState } from "react";
 
 const ProductsList: React.FC<JSX.Element> = () => {
   const [products, setProducts] = useState([]);
@@ -9,50 +9,40 @@ const ProductsList: React.FC<JSX.Element> = () => {
 
   useEffect(() => {
     const storedData = localStorage.getItem("products");
+
     if (storedData) {
       setCart(storedData);
       setProducts(JSON.parse(storedData));
     }
-  }, []);
+  }, [cart]);
 
-  const removeItemsFromCart = (productId:String, product:ProductType) => {
-    console.log(cart);
-    //const cartItems = Object.values(cart);
-    //const  = cartItems.findIndex((item: any) => item.id === product.id && item.quantity === product.quantity);
-    const productIndex:number = Number.parseInt(productId.toString());
-    
-    // if (productIndex === -1) {
-    //   console.log("nie znaleziono");
-    //   return;
-    // }
-  
-    if (cart[productIndex].quantity > 1) {
-      setCart((prevCart: any) =>
-        prevCart.map((item: any) =>
-          item.id === product.id && item.quantity === product.quantity
-            ? { ...item, quantity: item.quantity - 1 }
-            : item
-        )
-      );
-    } else {
-      setCart((prevCart: any) =>
-        prevCart.filter((item: any) => item.id !== product.id || item.quantity !== product.quantity)
-      );
+  const removeItemsFromCart = (productId: String, product: ProductType) => {
+    const duplicatedStorageData = localStorage.getItem("products");
+
+    if (duplicatedStorageData) {
     }
+
+    setCart((prev: any) => (Array.isArray(prev) ? prev?.filter(({ id }: { id: string }) => id !== productId) : prev));
+
+    setProducts((prev) => {
+      const newProducts: any = prev.filter(({ id }) => id !== productId);
+      localStorage.setItem("products", JSON.stringify(newProducts));
+      return newProducts;
+    });
   };
 
-  if (products === undefined || (Array.isArray(products) && products.length === 0))
-    return <div>No products available</div>;
+  if (products === undefined || (Array.isArray(products) && products.length === 0)) return <div>No products available</div>;
 
   return (
-    //otypowac
     <div className=" cart flex flex-col items-center">
       {products.map((product: ProductType, index: number) => (
         <div key={index} className=" cartElement flex items-center justify-between w-full">
           <div>
             {index + 1}. {product.name}
           </div>
-          <button className="ml-2" onClick={() => removeItemsFromCart(product.id, product)}>Remove</button>
+          <button className="ml-2" onClick={() => removeItemsFromCart(product.id, product)}>
+            Remove
+          </button>
         </div>
       ))}
     </div>
@@ -60,70 +50,3 @@ const ProductsList: React.FC<JSX.Element> = () => {
 };
 
 export default ProductsList;
-
-
-//try
-
-
-// "use client";
-// import React, { useEffect, useState } from "react";
-// import { useShopContext } from "@/providers/ShopContext";
-// import { ProductType } from "@/models/product";
-
-// const ProductsList: React.FC<JSX.Element> = () => {
-//   const { cart, setCart } = useShopContext();
-//   const [products, setProducts] = useState<any[]>([]);
-
-//   useEffect(() => {
-//     const storedData = localStorage.getItem("products");
-//     if (storedData) {
-//       setProducts(JSON.parse(storedData));
-//     }
-//   }, []);
-
-//   if (products === undefined || (Array.isArray(products) && products.length === 0))
-//     return <div>No products available</div>;
-
-//   const removeItemsFromCart = (product: ProductType) => {
-//     const productIndex = cart.findIndex(
-//       (item: any) => item.id === product.id && item.quantity === product.quantity
-//     );
-
-//     if (productIndex === -1) {
-//       return;
-//     }
-
-//     if (cart[productIndex].quantity > 1) {
-//       setCart((prevCart: any) =>
-//         prevCart.map((item: any) =>
-//           item.id === product.id && item.quantity === product.quantity
-//             ? { ...item, quantity: item.quantity - 1 }
-//             : item
-//         )
-//       );
-//     } else {
-//       setCart((prevCart: any) =>
-//         prevCart.filter(
-//           (item: any) => item.id !== product.id || item.quantity !== product.quantity
-//         )
-//       );
-//     }
-//   };
-
-//   return (
-//     <div className=" cart flex flex-col items-center">
-//       {products.map((product: any, index: number) => (
-//         <div key={index} className=" cartElement flex items-center justify-between w-full">
-//           <div>
-//             {index + 1}. {product.name}
-//           </div>
-//           <button className="ml-2" onClick={() => removeItemsFromCart(product)}>
-//             Remove
-//           </button>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ProductsList;
