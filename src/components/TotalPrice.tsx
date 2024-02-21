b99a199c95ad273350f628e3b1b2ae25cd62e8ff
MarceliEDU <marcelihryniuk.edu@gmail.com>
2024-02-22 00:46:17 +0100

    Spam fix

--- src/components/TotalPrice.tsx
+++ src/components/TotalPrice.tsx
@@ -11,8 +11,6 @@ const TotalPrice = () => {
 
     let total = cartType.reduce((acc, product) => acc + product.price, 0)
 
-    console.log("caty type:",cartType);
-
     return (
         <span className="navPrice p-2">
             {total.toFixed(2)} PLN
