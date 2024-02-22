1486f98372eaa58b770d8af4fa29917da00977a8
groh7 <pawel.pogroszewski1@wp.pl>
2024-02-22 01:19:45 +0100

    add-products styles

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -61,40 +61,70 @@ export default function AddProduct() {
   
 
   return (
-    <div>
-    <Navbar />
-    <div className="flex items-center justify-center">
-      <form className="text-center addItem" onSubmit={handleSubmit}>
-        <div>Nazwa:</div>
-        <input type="text" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
-        <div>Opis: </div>
-        <input
-          type="text"
-          value={formData.description}
-          onChange={(e) => setFormData({ ...formData, description: e.target.value })}
-        />
-        <div>Cena: </div>
-        <input
-          type="number"
-          value={formData.price}
-          onChange={(e) => setFormData({ ...formData, price: Number(e.target.value) })}
-        />
-        <div>Photo link: </div>
-        <input
-          type="text"
-          value={formData.photo}
-          onChange={(e) => setFormData({ ...formData, photo: e.target.value })}
-        />
-        <div>Dostępność w sztukach: </div>
-        <input
-          type="number"
-          value={formData.availability}
-          onChange={(e) => setFormData({ ...formData, availability: Number(e.target.value) })}
-        />
-        <div></div>
-        <input className="submit mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" value="Udostępnij produkt" />
-      </form>
-    </div>
+    <div className="w-full">
+      <Navbar />
+      <div className="flex items-center justify-center">
+        <form className="addItem-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Nazwa:</label>
+            <input
+              id="name"
+              type="text"
+              value={formData.name}
+              onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Opis:</label>
+            <input
+              id="description"
+              type="text"
+              value={formData.description}
+              onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="price">Cena:</label>
+            <input
+              id="price"
+              type="number"
+              value={formData.price}
+              onChange={(e) => setFormData({ ...formData, price: Number(e.target.value) })}
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+           <label htmlFor="photo">Zdjęcie: </label>
+            <input
+              id="photo"
+              type="text"
+              value={formData.photo}
+              onChange={(e) => setFormData({ ...formData, photo: e.target.value })}
+              placeholder="placeholder.png"
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="availability">Dostępność w sztukach:</label>
+            <input
+              id="availability"
+              type="number"
+              value={formData.availability}
+              onChange={(e) => setFormData({ ...formData, availability: Number(e.target.value) })}
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="submit bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="submit"
+              value="Udostępnij produkt"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
